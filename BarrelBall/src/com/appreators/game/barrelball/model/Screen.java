045764cee08bb6c74afc7d0d2e7a8b0001689073
keisuke <news.ko1986@gmail.com>
2014-06-22 13:47:22 +0900

    Screen作成中

---
+++ BarrelBall/src/com/appreators/game/barrelball/model/Screen.java
@@ -0,0 +1,30 @@
+package com.appreators.game.barrelball.model;
+
+public class Screen {
+	
+	Barrel barrel;
+	Ball ball;
+	Rail rail;
+	
+	public Barrel getBarrel() {
+		return barrel;
+	}
+	public void setBarrel(Barrel barrel) {
+		this.barrel = barrel;
+	}
+	public Ball getBall() {
+		return ball;
+	}
+	public void setBall(Ball ball) {
+		this.ball = ball;
+	}
+	public Rail getRail() {
+		return rail;
+	}
+	public void setRail(Rail rail) {
+		this.rail = rail;
+	} 
+		
+
+
+}
