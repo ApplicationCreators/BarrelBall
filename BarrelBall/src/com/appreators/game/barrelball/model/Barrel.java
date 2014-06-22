b6db76ee9177d30bd1d44313b2701691919d4005
keisuke <news.ko1986@gmail.com>
2014-06-22 14:34:07 +0900

    Barrel修正

--- BarrelBall/src/com/appreators/game/barrelball/model/Barrel.java
+++ BarrelBall/src/com/appreators/game/barrelball/model/Barrel.java
@@ -4,8 +4,40 @@ import com.appreators.game.barrelball.model.supers.Circle;
 
 public class Barrel extends Circle{
 
-	public Barrel(float x, float y, float radius) {
+	float[] rail_position;
+	float rail_width;
+	float rail_height;
+	
+	public Barrel(float x, float y, float radius, float rail_width, float rail_height) {
 		super(x, y, radius);
+		this.rail_position[0] = x;
+		this.rail_position[1] = y;
+		this.rail_width = rail_width;
+		this.rail_height = rail_height;
+	}
+	
+	public float[] getRail_position() {
+		return rail_position;
+	}
+
+	public void setRail_position(float[] rail_position) {
+		this.rail_position = rail_position;
+	}
+	
+	public float getRail_width() {
+		return rail_width;
+	}
+
+	public void setRail_width(float rail_width) {
+		this.rail_width = rail_width;
+	}
+
+	public float getRail_height() {
+		return rail_height;
+	}
+
+	public void setRail_height(float rail_height) {
+		this.rail_height = rail_height;
 	}
 
 	@Override
